233ebda66847a05a4215c52e7041ed11cb16680f
Tom Broughton <tom.a.broughton@gmail.com>
2018-12-11 00:55:14 +0000

    delete snapshot now only affects those with same retention setting

--- lambda/index.py
+++ lambda/index.py
@@ -13,29 +13,30 @@ def handler(event, context):
     client = boto3.client('lightsail')
     retention_days = int(getenv('RETENTION_DAYS', DEFAULT_RETENTION_DAYS))
     retention_period = timedelta(days=retention_days)
+    snapshot_suffix = getenv('SNAPSHOT_SUFFIX', AUTO_SNAPSHOT_SUFFIX)
 
-    _snapshot_instances(client)
-    _prune_snapshots(client, retention_period)
+    _snapshot_instances(client, snapshot_suffix)
+    _prune_snapshots(client, retention_period, snapshot_suffix)
 
 
-def _snapshot_instances(client, time=time, out=stdout):
+def _snapshot_instances(client, snapshot_suffix, time=time, out=stdout):
     for page in client.get_paginator('get_instances').paginate():
         for instance in page['instances']:
             snapshot_name = '{}-system-{}-{}'.format(instance['name'],
                                                      int(time() * 1000),
-                                                     AUTO_SNAPSHOT_SUFFIX)
+                                                     snapshot_suffix)
 
             client.create_instance_snapshot(instanceName=instance['name'],
                                             instanceSnapshotName=snapshot_name)
             print('Created Snapshot name="{}"'.format(snapshot_name), file=out)
 
 
-def _prune_snapshots(client, retention_period, datetime=datetime, out=stdout):
+def _prune_snapshots(client, retention_period, snapshot_suffix, datetime=datetime, out=stdout):
     for page in client.get_paginator('get_instance_snapshots').paginate():
         for snapshot in page['instanceSnapshots']:
             name, created_at = snapshot['name'], snapshot['createdAt']
             now = datetime.now(created_at.tzinfo)
-            is_automated_snapshot = name.endswith(AUTO_SNAPSHOT_SUFFIX)
+            is_automated_snapshot = name.endswith(snapshot_suffix)
             has_elapsed_retention_period = now - created_at > retention_period
 
             if (is_automated_snapshot and has_elapsed_retention_period):
